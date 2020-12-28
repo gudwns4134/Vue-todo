a5b8bab4173819482b3d13fcba9cc874d03276c5
hyeongjun <gudwns4134@naver.com>
2020-12-28 14:11:16 +0900

    vue-todo

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">   <!-- v-model로 data 값 연동 -->
+      <span class="addContainer" v-on:click="addTodo">
+          <i class="fas fa-plus addBtn"></i>
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function() {   // 저장만 하는 기능
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+            // 개발자모드 appliciton에 가면 localStorage 이곳에 key : value로 저장된다.
+        },                                                  // 기능을 따로 분할해서 설정
+        clearInput: function() {    // 지우는 기능
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float:right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="( todoItem, index ) in todoItems" v-bind:key="todoItem" class="shadow">
+            {{ todoItem }}
+            <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                <i class="fas fa-trash-alt"></i>
+            </span>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default { 
+    data : function () {
+        return {
+            todoItems: []
+        }
+    },
+    methods: {
+        removeTodo: function(todoItem, index) {
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);    // api 참고
+        }
+    },
+    created: function() { 
+        if(localStorage.length > 0){
+            for(var i = 0; i < localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){  
+                    // loglevel:webpack-dev-server 같지 않은 것들만 축력
+                    this.todoItems.push(localStorage.key(i))
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li {
+   display: flex; 
+   min-height: 50px;
+   height: 50px;
+   line-height: 50px;
+   margin: 0.5rem 0;
+   padding: 0 0.9rem;
+   background: white;
+   border-radius: 5px;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+}
+.checkBtn {
+    line-height: 45px;
+    color:#62acde;
+    margin-right: 5px;
+}
+.checkBtnComplated {
+    color:#b3adad;
+}
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
+
+</style>
